66bbc1c575b572644b77f111df9d7307bbb30526
Heiko Mueller <heiko.muller@gmail.com>
2017-07-02 16:12:29 -0400

    Add prediction set handle

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sco-datastore',
-    version='0.7.0',
+    version='0.8.0',
     description='Library to manipulate objects in the Standard Cortical Observer Data Store',
     keywords='neuroscience vision cortex ',
     author='Heiko Mueller',
